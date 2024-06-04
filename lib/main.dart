e35c1fce094d662ce4a0e562d90cd17b9a504585
mfatihy70 <mfatihyildiz1453@gmail.com>
2024-06-04 22:14:00 +0200

    small changes: removed a line and updated todo.txt

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,11 @@ import 'package:window_size/window_size.dart';
 import 'dart:io' show Platform;
 
 void main() {
-  //Check if this line is necessary:
-  WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isWindows) {
     setWindowTitle('Yildiz App');
     setWindowMinSize(const Size(420, 700));
-    
   }
+
   runApp(MyApp());
 }
 
